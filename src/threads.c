c9ae6816c652d4a82f4aaf956b18278f7c3f04a8
Alexander Klauer <Alexander.Klauer@googlemail.com>
2018-03-15 19:28:18 +0100

    Add dynlock callbacks

--- src/threads.c
+++ src/threads.c
@@ -1,4 +1,5 @@
 #include<stdatomic.h>
+#include<stdlib.h>
 #include<thread.h>
 
 /**
@@ -44,6 +45,62 @@ static _Thread_local unsigned char threadLocalDummy = 0;
  */
 static void threadid_func( CRYPTO_THREADID * dest ) {
 	CRYPTO_THREADID_set_pointer( dest, &threadLocalDummy );
+}
+
+static struct CRYPTO_dynlock_value * dynlock_create( char * file, int line ) {
+	struct CRYPTO_dynlock_value * result = malloc( sizeof( *result ) );
+	if ( result == NULL ) {
+		goto nodynlock;
+	}
+	int rc = mtx_init( &result->entryLock, mtx_plain );
+	if ( rc != thrd_success ) {
+		goto noelock;
+	}
+	rc = mtx_init( &result->writerLock, mtx_plain );
+	if ( rc != thrd_success ) {
+		goto nowlock;
+	}
+	result->numReaders = ATOMIC_VAR_INIT( 0 );
+
+	return result;
+
+	mtx_destroy( &result->writerLock );
+nowlock:
+	mtx_destroy( &result->entryLock );
+noelock:
+	free( result );
+nodynlock:
+	return NULL;
+}
+
+static void dynlock_destroy( struct CRYPTO_dynlock_value * lock, char * file, int line ) {
+	mtx_destroy( &lock->writerLock );
+	mtx_destroy( &lock->entryLock );
+	free( lock );
+}
+
+static void dynlock_lock( int mode, struct CRYPTO_dynlock_value * lock, char * file, int line ) {
+	if ( mode & CRYPTO_LOCK ) {
+		mtx_lock( &lock->entryLock );
+		if ( mode & CRYPTO_READ ) {
+			unsigned int old = atomic_fetch_add_explicit( &lock->numReaders, 1, memory_order_relaxed );
+			if ( old == 0 ) {
+				mtx_lock( &lock->writerLock );
+			}
+		} else {
+			mtx_lock( &lock->writerLock );
+		}
+		mtx_unlock( &lock->entryLock );
+	} else {
+		if ( mode & CRYPTO_READ ) {
+			unsigned int old = atomic_fetch_sub_explicit( &lock->numReaders, 1, memory_order_relaxed );
+			if ( old == 1 ) {
+				mtx_unlock( &lock->writerLock );
+			}
+		} else {
+			mtx_unlock( &lock->writerLock );
+		}
+	}
 }
 
 Error * e222crypto_threads_init( void ) {
